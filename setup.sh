eca819841cd356c34bb026abeacc683afbff589b
rbvto <rin.vto+git@gmail.com>
2018-09-06 15:03:39 -0300

    feat(general): remove surplus files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
-sudo apt-get intall -y virt-top
-sudo apt-get install sysstat
+#!/bin/bash
 
-sudo apt-get install python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install nodejs
+DEPENDENCIES=(virt-top sysstat python-software-properties python g++ make)
+
+apt -qq install -y $DEPENDENCIES
+
+( curl http://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1 && ./configure && make install ) # ok, fine, this step probably takes more than 30 seconds...
+( curl https://www.npmjs.org/install.sh | sh )
+
+npm install
